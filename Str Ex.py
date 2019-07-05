1c47b609c8bd7057b14cd691b129ec39c149cc1f
pataranonmart <52291297+pataranonmart@users.noreply.github.com>
2019-07-05 16:05:41 +0700

    Add files via upload
    str

---
+++ Str Ex.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+print("Hello")
+print(type("Hello"))
+print("Hello"+"world")
+print("Helloworld"*5)
+print(type("Helloworld"*5))
+
+
+# In[ ]:
+
+
+
+
