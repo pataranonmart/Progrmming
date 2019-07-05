d886091c0f8af4759c6bec615f1d1f4927b0f810
pataranonmart <52291297+pataranonmart@users.noreply.github.com>
2019-07-05 16:03:07 +0700

    Add files via upload

---
+++ BoolEX.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+print(True)
+print(False)
+print(True or False)
+print(True and False)
+print(type(True))
+print(type(False))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
