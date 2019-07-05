314c7950d8fa3a4ddd870852cf059cf46b944873
pataranonmart <52291297+pataranonmart@users.noreply.github.com>
2019-07-05 16:07:31 +0700

    Add files via upload

---
+++ รวม.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[47]:
+
+
+print("--------------------------------")
+print("Mart Shop")
+print("--------------------------------")
+print("water" ,3 , 10 ,"THB" ,3*10,"THB")
+print("Ole" ,10,10, "THB" ,10*10, "THB")
+print("Total" ,30+100, "THB")
+
+
+# In[ ]:
+
+
+
+
