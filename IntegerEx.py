41d883efae45f33d57eb921a7d2824b4097fd760
pataranonmart <52291297+pataranonmart@users.noreply.github.com>
2019-07-05 16:07:05 +0700

    Add files via upload
    int

---
+++ IntegerEx.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+print(type(18/2))
+print(type(18-2))
+print(type(18*2))
+print(type(18+2))
+print(18/2)
+print(18-2)
+print(18*2)
+print(18+2)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
