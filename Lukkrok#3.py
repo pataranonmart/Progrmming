077e9a6e21d7966867339ca15eac4f7a53a6ebe5
pataranonmart <52291297+pataranonmart@users.noreply.github.com>
2019-07-05 14:06:18 +0700

    Add files via upload
    โปรแกรมแรกครั้งแรกของมาร์ท

---
+++ Lukkrok#3.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+print("----------------------------------------")
+print("Mart Shop")
+print("----------------------------------------")
+print("Water      x 1   :10 THB")
+print("Ole          x 1   : 6  THB")
+print("Party       x 1    :20 THB")
+print("----------------------------------------")
+print("Total                :36 THB")
+
+
+# In[19]:
+
+
+print(type(10))
+print(type("Hello"))    
+print(type(True)) 
+print(type(10.3))
+print(type("1j"))   
+      
+
+
+# In[20]:
+
+
+print(10+3.5)
+
+
+# In[ ]:
+
+
+
+
