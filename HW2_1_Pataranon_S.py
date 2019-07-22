5f8854f2dec5853bb85d47d658337dcaf48cbd60
pataranonmart <52291297+pataranonmart@users.noreply.github.com>
2019-07-22 17:44:55 +0700

    Homework

---
+++ HW2_1_Pataranon_S.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#นาย ภัทรชนน แสงสุวรรณ 362515241012 EE36241N
+print("--------------Menu--------------")
+print("Welcome to Icecream Shop")
+print("1. Strawberry 80 THB")
+print("2. Vanila     50 THB")
+print("3. Chocolate  40 THB")
+print("4. Lemon      90 THB")
+print("5. Milk       10 THB")
+mart=int(input("What do you want?"))
+if mart==1 :
+    print("You order : 1 Strawberry 80 THB")
+elif mart==2 :
+    print("You order : 2 Vanila 50 THB")
+elif mart==3 :
+    print("You order : 3 Chocolate 40 THB")
+elif mart==4 :
+    print("You order : 4 Lemon 90 THB")
+elif mart==5 :
+    print("You order : 5 Milk 10 THB")
+
+
+# In[ ]:
+
+
+
+
