07a92e620204c79448f3beec168103c4eeb2eaa4
pataranonmart <52291297+pataranonmart@users.noreply.github.com>
2019-09-05 23:01:49 +0700

    Add files via upload

---
+++ Triangle3.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# นาย ภัทรชนน แสงสุวรรณ 362515241012 EE36241N
+max = 0
+i = 3 
+sum = 0
+for i in range(i):
+    angle = int(input("enter angle :"))
+    if angle > max:
+        max = angle
+    sum = sum + angle
+print("perimeter :", sum)
+print("hypotenuse", max)
+        
+
+
+# In[ ]:
+
+
+
+
