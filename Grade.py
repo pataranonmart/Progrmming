0aa888e47dc4a46f07c31961134914260470c244
pataranonmart <52291297+pataranonmart@users.noreply.github.com>
2019-09-05 22:58:28 +0700

    Add files via upload

---
+++ Grade.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#นาย ภัทรชนน แสงสุวรรณ 362515241012 EE36241N
+score = float(input("Fill out score :"))
+if score >100:
+    print("ERROR")
+elif score >=80:
+    print("A")
+elif score >=75:
+    print("B+")
+elif score >=70:
+    print("B")
+elif score >=65:
+    print("C+")
+elif score >=60:
+    print("C")
+elif score >=55:
+    print("D+")
+elif score >=50:
+    print("D")
+elif score <50:
+    print("F")
+else:
+    print("ERROR")
+
+
+# In[ ]:
+
+
+
+
