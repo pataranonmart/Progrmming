797e233e5579698eabb9fe8a26ff6c50e9f92448
pataranonmart <52291297+pataranonmart@users.noreply.github.com>
2019-09-05 23:00:15 +0700

    Add files via upload

---
+++ Temp.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#นาย ภัทรชนน แสงสุวรรณ 362515241012 EE36241N
+C = float(input("Fill out C : ") )
+F = ((9/5) * C) + 32
+K = C +  273.15
+print(F, "F")
+print(K, "K")
+
+
+# In[ ]:
+
+
+
+
