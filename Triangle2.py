2c3d89659a22d7c51e5726f38c9884844d68000a
pataranonmart <52291297+pataranonmart@users.noreply.github.com>
2019-09-05 23:00:50 +0700

    Add files via upload

---
+++ Triangle2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#นาย ภัทรชนน แสงสุวรรณ 362515241012 EE36241N
+import math
+A = float(input("Fill Out A :"))
+B = float(input("Fill Out B :"))
+D = float(input("Fill Out D :"))
+C = D*math.pi/180
+c = math.sqrt((a*a)+(b*b)-(2*a*b*math.cos(C)))
+print("c =", c, "cm.")
+
+
+# In[ ]:
+
+
+
+
