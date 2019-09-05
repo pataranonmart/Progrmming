475cd072af14de9abf1a38949789e0a0ed380158
pataranonmart <52291297+pataranonmart@users.noreply.github.com>
2019-09-05 22:53:41 +0700

    Add files via upload

---
+++ Avg.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# นาย ภัทรชนน แสงสุวรรณ 362515241012 EE36241N
+sum = 0
+count = 0
+n = float(input("enter number"))
+while n != -1:
+    sum += n
+    count = count + 1
+    n = float(input("enter number"))
+avg = sum / count
+print(avg)
+
+
+# In[ ]:
+
+
+
+
